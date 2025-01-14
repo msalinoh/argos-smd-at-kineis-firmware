0b0fe6a386b37513ce0bbbc1721c84d3274b1af5
FOURNIER Geoffrey <fournier.geoffrey77@gmail.com>
2025-01-14 15:42:25 +0400

    AT+TX in progress

---
+++ Kineis/App/Mcu/Src/mcu_spi_driver.c
@@ -0,0 +1,228 @@
+// SPDX-License-Identifier: no SPDX license
+/**
+ * @file    mcu_at_spi.c
+ * @author  Arribada
+ * @brief   MCU wrapper for AT SPI
+ */
+
+/**
+ * @addtogroup MCU_APP_WRAPPERS
+ * @brief MCU wrapper used by Kineis Application example.
+ *
+ * One has to implement API as per its microcontroller and its platform ressources.
+ * This version is for STM32 uC such as STM32WLE5xx, STM32WL55xx.
+ * @{
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include <stddef.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdio.h>
+#include "kineis_sw_conf.h" // for assert include below
+#include KINEIS_SW_ASSERT_H
+#include "mgr_log.h"
+#include "mcu_spi_driver.h"
+
+/* Defines -------------------------------------------------------------------*/
+#if defined(STM32WLE5xx) || defined(STM32WL55xx)
+#define USART_ISR_RXNE USART_ISR_RXNE_RXFNE
+#endif
+
+
+/* Variables -----------------------------------------------------------------*/
+
+static SPI_HandleTypeDef *hspi_handle;
+
+static uint8_t spiTxBuf[TXBUF_SIZE];
+static uint8_t spiRxBuf[RXBUF_SIZE];
+static SPI_Buffer rxBuf = { .data = spiRxBuf, .size = RXBUF_SIZE, .next_req = 1};
+static SPI_Buffer txBuf = { .data = spiTxBuf, .size = TXBUF_SIZE, .next_req = 0};
+
+
+// SPI Status Byte Definition
+uint8_t spi_status = (0 | SPI_OK);
+
+//static bool (*rxEvtCb)(uint8_t *pu8_RxBuffer, int16_t *pi16_nbRxValidChar);
+static int8_t (*rxSpiEvtCb)(SPI_Buffer *rx, SPI_Buffer *tx);
+
+/* Private function prototypes -----------------------------------------------*/
+
+
+// /* Functions -----------------------------------------------------------------*/
+// Callback when a command is received
+void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef *hspi) {
+    if (hspi->Instance == SPI1) {
+        MGR_LOG_DEBUG("RX completed\r\n");
+        //uint8_t command = spiRxBuf[0];
+        if (rxSpiEvtCb != NULL)
+        {
+        	//rxBuf.size = 1;
+        	rxSpiEvtCb(&rxBuf, &txBuf);
+
+//        	if (ret)
+//        	{
+//        		if (txBuf.next_req > 1)
+//        		{
+//					spi_status = SPI_OK | SPI_TX_WAITING;
+//	//        		HAL_SPI_Transmit_IT(hspi, txBuf.data, txBuf.next_req);
+//					HAL_SPI_TransmitReceive_IT(hspi, txBuf.data, rxBuf.data, txBuf.next_req);
+//        		} else {
+//					spi_status = SPI_OK | SPI_TX_WAITING;
+//					HAL_SPI_Transmit_IT(hspi, txBuf.data, txBuf.next_req);
+//        		}
+//        	} else {
+//        		//Re start receive command if an error occured
+//				rxBuf.next_req = 1;
+//				HAL_SPI_Receive_IT(hspi, rxBuf.data, rxBuf.next_req);
+//        	}
+        } else {
+			MGR_LOG_DEBUG("%s:: rxSpiEvtCb not defined\r\n", __func__);
+        }
+
+
+    }
+}
+
+// Callback for SPI TxRx completion (optional for debugging)
+void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi) {
+    if (hspi->Instance == SPI1) {
+        MGR_LOG_DEBUG("TX-RX completed\r\n");
+    }
+	spi_status = SPI_OK;
+	txBuf.next_req = 0;
+	rxBuf.next_req = 1;
+	HAL_SPI_Receive_IT(hspi, rxBuf.data, rxBuf.next_req);
+}
+
+void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef *hspi) {
+    if (hspi->Instance == SPI1) {
+        MGR_LOG_DEBUG("TX completed\n\r");
+
+    }
+}
+
+HAL_StatusTypeDef MCU_SPI_DRIVER_resp(uint8_t cmd)
+{
+	HAL_StatusTypeDef ret = HAL_OK;
+	if (cmd != 0)
+	{
+		//if(txBuf.next_req == 1)
+		//{
+			// waiting Read request
+			spi_status = SPI_OK | SPI_TX_WAITING;
+			ret = HAL_SPI_TransmitReceive_IT(hspi_handle, txBuf.data, rxBuf.data, txBuf.next_req);
+		//}
+	}
+	return ret;
+}
+HAL_StatusTypeDef MCU_SPI_DRIVER_wait_next(uint8_t cmd)
+{
+	HAL_StatusTypeDef ret = HAL_OK;
+//	if (cmd != 0)
+//	{
+		if(rxBuf.next_req == 1)
+		{
+			ret = HAL_SPI_Receive_IT(hspi_handle, rxBuf.data, rxBuf.next_req);
+			ret = HAL_OK;
+		}
+		if(rxBuf.next_req > 0)
+		{
+			spi_status = SPI_OK | SPI_RX_WAITING;
+			ret = HAL_SPI_Receive_IT(hspi_handle, rxBuf.data, rxBuf.next_req);
+		} else {
+			MGR_LOG_DEBUG("%s:: Waiting RX is %u should be greater than 0\n\r",__func__, rxBuf.next_req);
+			ret = HAL_ERROR;
+		}
+//	}
+//	else {
+//		MGR_LOG_DEBUG("%s:: cmd  is %u should be different than 0\n\r",__func__, rxBuf.next_req);
+//		ret = HAL_ERROR;
+//	}
+	return ret;
+}
+
+
+
+bool MCU_SPI_DRIVER_register(void *handle, int8_t (*rx_spi_evt_cb)(SPI_Buffer *rx, SPI_Buffer *tx))
+{
+	MGR_LOG_DEBUG("%s:: called\n\r", __func__);
+	hspi_handle = (SPI_HandleTypeDef *)handle;
+	// Set SPI OK and TX WAITING flags
+	spi_status = SPI_OK;
+	HAL_StatusTypeDef ret = HAL_OK;
+	txBuf.next_req = 0;
+	rxBuf.next_req = 1;
+	// Start receiving the command
+	ret = HAL_SPI_Receive_IT(hspi_handle, rxBuf.data, rxBuf.next_req);
+	//ret = HAL_SPI_TransmitReceive_IT(hspi_handle, txBuf.data, rxBuf.data, rxBuf.next_req);
+    if (ret == HAL_OK) // Want to read the command received
+    {
+    	if (rx_spi_evt_cb != NULL)
+    	{
+			rxSpiEvtCb = rx_spi_evt_cb;
+    	}
+    	return true;
+    } else {
+    	return false;
+    }
+}
+
+
+
+/**
+  * @brief  UART error callback. Can raise in case of UART OVERFLOW, DMA RX ERROR, ...
+ *
+ * This function is highly based on STM32HAL_UART. It overrides the generic defined error callback
+ *
+ * @note So far, this callback is emptied
+ *
+  * @param  huart UART handle.
+  * @retval None
+  */
+void HAL_SPI_ErrorCallback(__attribute__((unused)) SPI_HandleTypeDef *hspi)
+{
+
+	// Set SPI OK and TX WAITING flags
+	spi_status = 0;
+	MGR_LOG_DEBUG("%s:: called\n\r", __func__);
+    // Check for Overrun Error
+    if (hspi->ErrorCode & HAL_SPI_ERROR_OVR) {
+        // Clear the Overrun flag
+		MGR_LOG_DEBUG("%s:: Clear Overrun flag\n\r", __func__);
+        __HAL_SPI_CLEAR_OVRFLAG(hspi);
+    }
+
+    // Check for Mode Fault Error
+    if (hspi->ErrorCode & HAL_SPI_ERROR_MODF) {
+        // Clear the Mode Fault flag
+		MGR_LOG_DEBUG("%s:: Clear Mode Fault flag\n\r", __func__);
+        __HAL_SPI_CLEAR_MODFFLAG(hspi);
+    }
+
+    // Check for Frame Error
+    if (hspi->ErrorCode & HAL_SPI_ERROR_FRE) {
+        // Clear the Frame Error flag
+		MGR_LOG_DEBUG("%s:: Clear Frame Error flag\n\r", __func__);
+        __HAL_SPI_CLEAR_FREFLAG(hspi);
+    }
+
+    // Reset the ErrorCode
+    hspi->ErrorCode = HAL_SPI_ERROR_NONE;
+
+    // Reset the SPI state
+    hspi->State = HAL_SPI_STATE_READY;
+
+    // Optionally reinitialize SPI
+    HAL_SPI_DeInit(hspi);
+    HAL_SPI_Init(hspi);
+    MCU_SPI_DRIVER_register(hspi, NULL);
+
+}
+
+/**
+ * @}
+ */
--- build_info.c
+++ build_info.c
@@ -1,3 +1,3 @@
 #include "build_info.h"
 
-const char uc_fw_vers_commit_id[] = "4f12936*_DMp_gui_basic";
+const char uc_fw_vers_commit_id[] = "dc42e9d*_DVMp_gui_basic";
